bbfa7b3d69ac23e40017b6ca973b9fedabed4d12
Justin Roeder <92489099+delphijustin@users.noreply.github.com>
2022-11-10 16:18:38 -0600

    Update mircsend.bat
    Testing email settings can be done by just running the mircsend.bat with no parameters

--- mircsend.bat
+++ mircsend.bat
@@ -5,5 +5,13 @@ set smtp_server=
 set sendto=
 set smtp_port=587
 set smtp_options=/SSL
+set logfile=%tmp%\mircmail.log
 timeout /T 5
-c:\SwithMail.exe /s /from "%from%" /u "%smtp_user%" /pass "%smtp_pass%" /server "%smtp_server%" /p "%smtp_port%" %smtp_options% /to "%sendto%" /sub %2 /b %1 /Log "%tmp%\mircmail.log"
+if "%1"=="" goto test
+c:\SwithMail.exe /s /from "%from%" /u "%smtp_user%" /pass "%smtp_pass%" /server "%smtp_server%" /p "%smtp_port%" %smtp_options% /to "%sendto%" /sub %2 /b %1 /Log "%logfile%" 
+goto done
+:test
+c:\SwithMail.exe /s /from "%from%" /u "%smtp_user%" /pass "%smtp_pass%" /server "%smtp_server%" /p "%smtp_port%" %smtp_options% /to "%sendto%" /sub "Test Message(%RANDOM%)" /b "%USERNAME% on %COMPUTERNAME%" /Log "%logfile%"
+type %logfile%
+pause
+:done
