bfe9a6e061b94266c42f5e3f29fa538c7e0de5e7
Justin Roeder <92489099+delphijustin@users.noreply.github.com>
2022-11-10 20:47:28 -0600

    Update mircsend.bat

--- mircsend.bat
+++ mircsend.bat
@@ -7,11 +7,4 @@ set smtp_port=587
 set smtp_options=/SSL
 set logfile=%tmp%\mircmail.log
 timeout /T 5
-if "%1"=="" goto test
 c:\SwithMail.exe /s /from "%from%" /u "%smtp_user%" /pass "%smtp_pass%" /server "%smtp_server%" /p "%smtp_port%" %smtp_options% /to "%sendto%" /sub %2 /b %1 /Log "%logfile%" 
-goto done
-:test
-c:\SwithMail.exe /s /from "%from%" /u "%smtp_user%" /pass "%smtp_pass%" /server "%smtp_server%" /p "%smtp_port%" %smtp_options% /to "%sendto%" /sub "Test Message(%RANDOM%)" /b "%USERNAME% on %COMPUTERNAME%" /Log "%logfile%"
-type %logfile%
-pause
-:done
